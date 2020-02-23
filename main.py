149b3759b8231ac52f134a74cd4e115dde2a2b04
Karriem Gimadeev <listentowoes@gmail.com>
2020-02-23 18:09:27 +0300

    upd

--- main.py
+++ main.py
@@ -3,72 +3,63 @@ from bs4 import BeautifulSoup as bs
 import re
 
 
+# TODO singleton
 class Scraper:
     def __init__(self, baseurl, filename, rubric=None, date=None):
-        self.baseurl   = baseurl
-        self.filename  = filename
-        self.rubric    = rubric
-        self.date      = date
-        self._session   = requests.Session()
-        self._rq        = requests.get(self.baseurl)
-        self._soup      = bs(self._rq.content, "lxml")
+        self.baseurl = baseurl
+        self.filename = filename
+        self.rubric = rubric
+        self.date = date
+        self._session = requests.Session()
+        self._rq = requests.get(self.baseurl)
+        self._soup = bs(self._rq.content, "lxml")
 
-    def getNewsList(self):
-        newsSpans = self._soup.findAll("div", attrs={"class": "news"})
+    def getPostList(self, posttype):
+        c = "news" if posttype == "news" else "article"
+        newsSpans = self._soup.findAll("div", attrs={"class": c})
         newsList = []
         for span in newsSpans:
             href = span.find("a")["href"]
             if self.date is None:
-                if re.match("/news/\d{4}/\d{2}/\d{2}/.+/", href):
+                if re.match("/" + posttype + "/\d{4}/\d{2}/\d{2}/.+/", href):
                     newsList.append(self.baseurl + href)
             else:
-                if re.match("/news/" + self.date.replace(".", "/") + "/.+/", href):
+                if re.match("/" + posttype + "/" + self.date.replace(".", "/") + "/.+/", href):
                     newsList.append(self.baseurl + href)
         return newsList
 
-    def getArticlesList(self):
-        articlesSpans = self._soup.findAll("div", attrs={"class": "article"})
-        articlesList = []
-        for span in articlesSpans:
-            href = span.find("a")["href"]
-            if self.date is None:
-                if re.match("/articles/\d{4}/\d{2}/\d{2}", href):
-                    articlesList.append(self.baseurl + href)
-            else:
-                if re.match("/articles/" + self.date.replace(".", "/") + "/.+/", href):
-                    articlesList.append(self.baseurl + href)
-        return articlesList
-
-    def getBriefList(self):
-        briefSpans = self._soup.findAll("div", attrs={"class": "article"})
-        briefList = []
-        for span in briefSpans:
-            href = span.find("a")["href"]
-            if self.date is None:
-                if re.match("/brief/\d{4}/\d{2}/\d{2}", href):
-                    briefList.append(self.baseurl + href)
-            else:
-                if re.match("/brief/" + self.date.replace(".", "/") + "/.+/", href):
-                    briefList.append(self.baseurl + href)
-        return briefList
-
     def getUrlList(self):
         if self.rubric is None:
-            return self.getNewsList() + self.getArticlesList() + self.getBriefList()
-        elif self.rubric == "news":
-            return self.getNewsList()
-        elif self.rubric == "articles":
-            return self.getArticlesList()
+            return self.getPostList("news") + self.getPostList("articles") + self.getPostList("brief")
+        else:
+            return self.getPostList(self.rubric)
 
-# class Post:
-# class News(Post):
-# class Brief(Post):
-# class Article(Post):
 
-# LINK TITLE CONTENT
+class Post:
+    def __init__(self, url):
+        self.url = url
+        self._rq = requests.get(self.url)
+        self._soup = bs(self._rq.content, "lxml")
+        self.headline = None
+        self.subheads = None
+        self.cont = None
+
+    @property
+    def getHeadline(self):
+        return self._soup.find("h1", attrs={"itemprop": "headline"}).text
+
+    @property
+    def content(self):
+        return self._soup.find("div", attrs={"itemprop": "articleBody"}).text
+
 
 def main():
-    scraper = Scraper("https://lenta.ru/", filename="example.txt")
-    print(scraper.getBriefList())
-if  __name__ == "__main__":
+    scraper = Scraper("https://lenta.ru/", filename="example.txt", rubric="articles")
+    print(Post(scraper.getUrlList()[0]).content)
+    # with open("./example.txt", "w") as outfile:
+    #     for n in scraper.getUrlList():
+    #         outfile.write(Post(n).content.text)
+
+
+if __name__ == "__main__":
     main()
